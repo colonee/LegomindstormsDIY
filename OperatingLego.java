5a0eef1d5b0bfd2ed60aad8acb182273fe2ae3ff
colonee <colonee11@gmail.com>
2016-07-22 16:35:53 +0900

    ひな型の作成
    とりあえすメソッドを作っていくまで

--- OperatingLego.java
+++ OperatingLego.java
@@ -6,59 +6,74 @@ public class OperatingLego {
 	}
 	void main(){
 		init();			//初期化処理
+		connectBT();	//BlueTooth接続
 		while(!isGoal()){	//ゴールにつくまで処理を回す
-			processCommand();
+			processCommand(); //命令を読み込んで処理する
 		}
 		del();		//終了処理
-		
 	}
-	//
+	//初期化
 	void init(){
 		
 	}
+	// BTConnection作成
+	void connectBT(){
+		
+	}
 	
-	void startCommandProcessing(){
-		
-	}
-	void finishCommandProcessing(){
-		
-	}
+//	void startCommandProcessing(){
+//		
+//	}
+//	void finishCommandProcessing(){
+//		
+//	}
+	// 命令を読み込んで処理する
 	void Processcommand(){
 		
 	}
+	//左の回転数を上げる
 	void increaseLeftWheelSpeed(){
 		
 	}
+	//左の回転数を下げる
 	void decreaseLeftWheelSpeed(){
 		
 	}
+	//右の回転数を上げる
 	void increaseRightWheelSpeed(){
 		
 	}
+	//右の回転数を下げる
 	void decreaseRightWheelSpeed(){
 		
 	}
+	//立ち入り禁止エリアを出る
 	void avoidKeepOutArea(){
 		
 	}
+	//avoidKeepOutArea後の車を止める
 	void stopCar(){
 		
 	}
-	//ゴールしてるかどうか？
+	//avoidKeepOutArea後の車の再開
+	void RunCar(){
+		
+	}
+	//ゴールしてるかどうかの判別
 	boolean isGoal(){
 		return null;
 	}
 	
-	
-	void RunCar(){
-		
-	}
+
 	//車輪の速度を初期化する
 	void initializeCarWheel(){
 		
 	}
-	//処理を終了する
+	//処理を正常に終了する
 	void del(){
 		
 	}
+}
+class ReceiveCommand extends Thread{
+	public 
 }
